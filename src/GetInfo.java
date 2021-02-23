283214fbad2854c5da0aeb960f5867295d6f2920
Isaac Steele <isaacsteele@Isaacs-MacBook-Pro.local>
2021-02-23 18:24:51 -0500

    Added a greeter method

--- src/GetInfo.java
+++ src/GetInfo.java
@@ -14,6 +14,20 @@ public class GetInfo {
         System.out.printf("Your name is: %s \nYour age is: %d", name, age);
     }
 	
+	public static void printGreeting(String name, int hour) {
+		if (hour < 12) {
+			System.out.println("Good morning, " + name + "!");
+		}
+		
+		if (hour >= 12 & hour < 18) {
+			System.out.println("Good afternoon, " + name + "!");
+		}
+		
+		if (hour >= 18) {
+			System.out.println("Good evening, " + name + "!");
+		}
+	}
+	
 	public static void main(String[] args) {
 		getInfo();
 	}
